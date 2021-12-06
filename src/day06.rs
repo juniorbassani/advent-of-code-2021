736ed660566d701a7d2dcf9e310ac18a955055ae
Júnior Bassani <junior.eduardo.bassani@gmail.com>
2021-12-06 11:02:11 -0300

    Day 06

---
+++ src/day06.rs
@@ -0,0 +1,56 @@
+use once_cell::sync::Lazy;
+
+use crate::utils;
+
+static INPUT: Lazy<Vec<u8>> = Lazy::new(|| utils::parse_line(INPUT_PATH));
+const INPUT_PATH: &str = "input/day06";
+
+fn number_of_fishes_after_n_days(fish_list: &[u8], days: usize) -> usize {
+    let mut fishes = [0; 9];
+    for &fish in fish_list {
+        fishes[fish as usize] += 1;
+    }
+
+    for _ in 0..days {
+        let to_procreate = fishes[0];
+
+        for i in 0..fishes.len() - 1 {
+            fishes[i] = fishes[i + 1];
+        }
+
+        fishes[8] = to_procreate;
+        fishes[6] += to_procreate;
+    }
+
+    let mut sum = 0;
+    for fish in fishes {
+        sum += fish;
+    }
+    sum
+}
+
+pub fn part1() -> usize {
+    number_of_fishes_after_n_days(&*INPUT, 80)
+}
+
+pub fn part2() -> usize {
+    number_of_fishes_after_n_days(&*INPUT, 256)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &[u8] = &[3, 4, 3, 1, 2];
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(number_of_fishes_after_n_days(INPUT, 18), 26);
+        assert_eq!(number_of_fishes_after_n_days(INPUT, 80), 5934);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(number_of_fishes_after_n_days(INPUT, 256), 26984457539);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,28 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 mod utils;
 
 fn main() {
-    println!("Day 01 [part 01]: {}", day01::part1());
-    println!("Day 01 [part 02]: {}\n", day01::part2());
+    // println!("Day 01 [part 01]: {}", day01::part1());
+    // println!("Day 01 [part 02]: {}\n", day01::part2());
 
-    println!("Day 02 [part 01]: {}", day02::part1());
-    println!("Day 02 [part 02]: {}\n", day02::part2());
+    // println!("Day 02 [part 01]: {}", day02::part1());
+    // println!("Day 02 [part 02]: {}\n", day02::part2());
 
-    println!("Day 03 [part 01]: {}", day03::part1());
-    println!("Day 03 [part 02]: {}\n", day03::part2());
+    // println!("Day 03 [part 01]: {}", day03::part1());
+    // println!("Day 03 [part 02]: {}\n", day03::part2());
 
-    println!("Day 03 [part 01]: {}", day03::part1());
-    println!("Day 03 [part 02]: {}\n", day03::part2());
+    // println!("Day 03 [part 01]: {}", day03::part1());
+    // println!("Day 03 [part 02]: {}\n", day03::part2());
 
-    println!("Day 04 [part 01]: {}", day04::part1());
-    println!("Day 04 [part 02]: {}\n", day04::part2());
+    // println!("Day 04 [part 01]: {}", day04::part1());
+    // println!("Day 04 [part 02]: {}\n", day04::part2());
 
-    println!("Day 05 [part 01]: {}", day05::part1());
-    println!("Day 05 [part 02]: {}", day05::part2());
+    // println!("Day 05 [part 01]: {}", day05::part1());
+    // println!("Day 05 [part 02]: {}", day05::part2());
+
+    println!("Day 06 [part 01]: {}", day06::part1());
+    println!("Day 06 [part 02]: {}", day06::part2());
 }
